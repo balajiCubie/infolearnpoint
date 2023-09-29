49badb7285ab251e5a2dfa692506ecdb8bdc8ddf
Balaji K <balajibala76394@gmail.com>
2023-09-29 11:31:57 +0530

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+
+// Remove this if you're not using Fullcalendar features
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+}
