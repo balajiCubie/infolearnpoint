fea7380e35773cc8714c391dbc2156cfdf7538b2
Balaji K <balajibala76394@gmail.com>
2023-09-29 10:36:58 +0530

    home

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+
+import styles from '@/styles/Home.module.css'
+export default function Home() {
+  return (
+    <>
+    <h1 className={styles.description}>hello world</h1>
+    <h1>hello world</h1>
+    <h1>hello world</h1>
+          </>
+  )
+}
