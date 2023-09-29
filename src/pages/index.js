c6b45dc47d1f7d5340e6387678fb11f1bfb13ada
Balaji K <balajibala76394@gmail.com>
2023-09-29 10:57:51 +0530

    cuvisoft

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
     <>
-    <h1 className={styles.description}>hello world</h1>
-    <h1>hello world</h1>
-    <h1>hello world</h1>
+    <h1>Hello from cuvisoft</h1>
+    <h1>Infolearnpoint</h1>
+    
           </>
   )
 }
