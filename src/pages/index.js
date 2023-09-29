73e97ac5b477fcee96ad4a4422191dea81c7a462
Balaji K <balajibala76394@gmail.com>
2023-09-29 11:23:06 +0530

    info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,38 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
 
-import styles from '@/styles/Home.module.css'
-export default function Home() {
+const Home = () => {
   return (
-    <>
-    <h1>Hello from cuvisoft</h1>
-    <h1>Infolearnpoint</h1>
-    
-          </>
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Info learn point Kick start by cuvisoft 🚀'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
+            <Typography>
+              Please make sure to read our Template Documentation to understand where to go from here and how to use our
+              template.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='ACL and JWT 🔒'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>
+              Access Control (ACL) and Authentication (JWT) are the two main security features of our template and are implemented in the starter-kit as well.
+            </Typography>
+            <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
+
+export default Home
