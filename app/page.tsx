bd9871413d0fba8082e60db1820b368d8f1a6269
Balaji K <balajibala76394@gmail.com>
2024-09-22 07:54:58 +0530

    dir change

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-import Image from "next/image";
-import Header from "../components/Home/header"
-import SubHeader from "@/components/Home/subHeader";
-import ListCard from "@/components/Home/listCard";
-import Footer from "@/components/Home/footer";
+import Header from "./components/Home/header";
+import SubHeader from "./components/Home/subHeader";
+import ListCard from "./components/Home/listCard";
+import Footer from "./components/Home/footer";
+
 export default function Home() {
   return (
     <>
-<Header/>
-<SubHeader/>
-<ListCard/>
-<Footer/>
-
-
-
+      <Header />
+      <SubHeader />
+      <ListCard />
+      <Footer />
     </>
-    
-    
   );
 }
