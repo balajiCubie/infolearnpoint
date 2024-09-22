9b727b0a82ed90c8478b128b703e0a3b40fc8881
Balaji K <balajibala76394@gmail.com>
2024-09-22 10:55:02 +0530

    flie location

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Link from 'next/link';
-import Navbar from "./components/Home/NavBar";
-import SubHeader from "./components/Home/SubHeader";
-import LearnToCodeHeader from './components/Home/LearnToCodeHeader';
+import Navbar from "../components/Home/NavBar";
+import SubHeader from "../components/Home/SubHeader";
+import LearnToCodeHeader from '../components/Home/LearnToCodeHeader';
 
 export default function Home() {
   return (
